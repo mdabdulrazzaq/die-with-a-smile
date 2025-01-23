1f062109c945602c7d14ea8974396a323c55472d
Devalla Divya Sree Durga <devalladivyasreedurga@Devallas-MacBook-Air.local>
2025-01-23 00:24:21 -0600

    Sprint-1: init, Video socket connection establishment

---
+++ die-with-a-smile/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoChat from "./Components/VideoChat";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Die with a Smile - Video Game</h1>
+      <VideoChat />
+    </div>
+  );
+};
+
+export default App;
