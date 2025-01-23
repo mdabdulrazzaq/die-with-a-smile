97b387e392b40a0148c3bcd48e0c16b993a1e6ec
Mohammad Abdul Razzaq <53903436+mdabdulrazzaq@users.noreply.github.com>
2025-01-23 02:43:56 -0500

    be - integrated

--- die-with-a-smile/src/Components/VideoChat.js
+++ die-with-a-smile/src/Components/VideoChat.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import Peer from "simple-peer";
 import { v4 as uuidV4 } from "uuid";
 
-const socket = io("http://localhost:5001");
+const socket = io("https://die-with-a-smile-production.up.railway.app");
 
 const VideoChat = () => {
   const [roomId, setRoomId] = useState("");
